c3abd6293e5a308e84b364c324cb81bec915f967
David Stockton <dave@davidstockton.com>
2014-02-26 22:37:30 -0700

    Updates to deploy script

--- public/deploy.php
+++ public/deploy.php
@@ -1,5 +1,5 @@
 <?php
-$code = require_once __DIR__ . '/../../phpstudygroup.com.key';
+$code = include __DIR__ . '/../../phpstudygroup.com.key';
 
 if ($_GET['code'] != $code) {
     echo 'Deploy key invalid.';
